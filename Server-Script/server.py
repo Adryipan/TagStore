46aa563c51c48710d8aef767f81fa6f070f78b13
Sandy <hpan0013@student.monash.edu>
2020-06-05 15:50:49 +1000

    updated server script

--- Server-Script/server.py
+++ Server-Script/server.py
@@ -1,4 +1,4 @@
-from flask import Flask,request, render_template, redirect
+from flask import Flask,request, render_template, redirect,flash
 import requests
 import json
 from werkzeug.utils import secure_filename
@@ -15,49 +15,51 @@ def allowed_file(filename):
 
 @app.route("/uploadImage", methods=["POST"])
 def uploadImage():
-    # If the post request has the file
-    if "image" not in request.files:
-        flash("No file part")
-        return redirect(request.url)
-    file = request.files["image"]
-    # if the user does not select a file
-    if file.filename == "":
-        flash("No selected file")
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        #Add the code to upload the image to the S3 budket here
-	#Add code here
-        return redirect("index.html")
+    if not id_token:
+        return "Invalid Request"
+    else:
+        # If the post request has the file
+        if "image" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["image"]
+        # if the user does not select a file
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            #Add the code to upload the image to the S3 budket here
+            #Add code here
+            return redirect("index.html")
 
 
 @app.route("/searchTag", methods=["POST"])
 def searchTag():
     # Take t user input, split it with comma and assign it to a list
-    text = request.form["tags"]
-    processed_text = text.lower()
-    tagList = processed_text.split(",")
-    s=''
-    parm={}
-    count=1
-    for i in tagList:
-        parm['tag{}'.format(count)]=i
-        count+=1
-        #if not s:
-        #    s+="tag{}={}".format(count,i)
-        #else:
-        #    s+="&tag{}={}".format(count,i)
-    objURL="https://8wl1a323h1.execute-api.us-east-1.amazonaws.com/prod/api/search"
-    header={'auth':access_token}
-    resp=requests.get(url=objURL,headers=header,params=parm)
-    #print(resp.json())
-    objSTRING=[]
-    count1=1
-    for i in resp.json():
-        objSTRING.append(i['url{}'.format(count1)])
-        count1+=1
-    JSON={'url':str(objSTRING)}
-    return JSON
+    if not access_token:
+        return "Invalid Request"
+    else:
+        text = request.form["tags"]
+        processed_text = text.lower()
+        tagList = processed_text.split(",")
+        s=''
+        parm={}
+        count=1
+        for i in tagList:
+            parm['tag{}'.format(count)]=i
+            count+=1
+        objURL="https://8wl1a323h1.execute-api.us-east-1.amazonaws.com/prod/api/search"
+        header={'auth':access_token}
+        resp=requests.get(url=objURL,headers=header,params=parm)
+        #print(resp.json()
+        objSTRING=[]
+        count1=1
+        for i in resp.json():
+            objSTRING.append(i['url{}'.format(count1)])
+            count1+=1
+        JSON={'url':str(objSTRING)}
+        return JSON
     
 @app.route("/getHomeTemp", methods=["GET"])
 def getHomeTemp():
@@ -75,18 +77,25 @@ def getUploadTemplate():
 @app.route("/")
 def hello():
     code=request.args.get('code')
-    tokURL="https://fit5225-a2.auth.us-east-1.amazoncognito.com/oauth2/token"
-    Data = {'grant_type':'authorization_code',
-            'client_id':'26o7svq9f4d9nktrkkapicdg50',
-            'redirect_uri':'https://3.209.221.168:4000',
-            'code':code}
-    tok=requests.post(url=tokURL,data=Data)
-    res=tok.json()
-    global id_token
-    id_token=res['id_token']
-    global access_token
-    access_token=res['access_token']
-    return render_template("index.html")
+    if code:
+        tokURL="https://fit5225-a2.auth.us-east-1.amazoncognito.com/oauth2/token"
+        Data = {'grant_type':'authorization_code',
+                'client_id':'26o7svq9f4d9nktrkkapicdg50',
+                'redirect_uri':'https://3.209.221.168:4000',
+                'code':code}
+        tok=requests.post(url=tokURL,data=Data)
+        if tok.ok:
+            res=tok.json()
+            global id_token
+            id_token=res['id_token']
+            global access_token
+            access_token=res['access_token']
+            print(id_token)
+            return render_template("index.html")
+        else:
+            return "Invalid Code"
+    else:
+        return "Invalid Request"
 
 if __name__ == "__main__":
     app.run(ssl_context=('certificate/cert.pem', 'certificate/key.pem'), host='0.0.0.0', port=4000, threaded=True, debug=True)
